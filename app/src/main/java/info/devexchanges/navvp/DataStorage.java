3941a17a33e8f16aef2d556f4ff934928162e5d1
nejek16 <kozjek.nejc198@gmail.com>
2019-05-14 14:27:03 +0200

    datastorage update

--- app/src/main/java/info/devexchanges/navvp/DataStorage.java
+++ app/src/main/java/info/devexchanges/navvp/DataStorage.java
@@ -29,26 +29,52 @@ public class DataStorage {
     private Context context;
     private File file;
 
+    //Paths
+    private static final String basicData  = "ATbasicData.json";
+    private static final String consumed  = "ATconsumed.json";
+    private static final String drinks  = "ATdrinks.json";
+
     public DataStorage(Context context){
         this.context=context;
     }
 
     public boolean isFirstOpen(){
         try {
-            JSONObject json=new JSONObject(readFile());
+            JSONObject json=new JSONObject(readFile(basicData));
             return false;
         } catch (JSONException e) {
-            writeFile("{\"consumed\":[], \"drinks\":[], \"gender\":null, \"weight\":null, \"age\":null}");
+            writeFile("{\"gender\":null, \"weight\":null, \"age\":null}","ATbasicData.json");
             return true;
 
         }
 
     }
 
+    public void setAge(int age){
+        try {
+            JSONObject json =new JSONObject(readFile(basicData));
+            json.put("age",age);
+        } catch (JSONException e) {
+            Toast.makeText(context,"ERROR: Data storage failed!",Toast.LENGTH_LONG);
+            e.printStackTrace();
+        }
+    }
+
+    public int getAge(){
+        try {
+            JSONObject json =new JSONObject(readFile(basicData));
+            return json.getInt("age");
+        } catch (JSONException e) {
+            Toast.makeText(context,"ERROR: Data storage failed!",Toast.LENGTH_LONG);
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
     public List<JSONObject> getDataGetConsumed(){
         List<JSONObject> allConsumed=new ArrayList<JSONObject>();
         try {
-            JSONObject json =new JSONObject(readFile());
+            JSONObject json =new JSONObject(readFile("ATconsumed.json"));
 
             JSONArray cons =new JSONArray(json.get("consumed").toString());
             //cons.put("{\"drink\":\"Lasko\"}");
@@ -67,7 +93,7 @@ public class DataStorage {
      */
     private int getIdConsumed(){
         try {
-            JSONObject json =new JSONObject(readFile());
+            JSONObject json =new JSONObject(readFile("ATconsumed.json"));
 
             JSONArray cons =new JSONArray(json.get("consumed").toString());
             String id=new JSONObject(cons.get(cons.length()).toString()).get("id").toString();
@@ -79,11 +105,20 @@ public class DataStorage {
 
     }
 
+    public void test(){
+        try {
+            JSONArray a=new JSONArray("[]");
+            Toast.makeText(context,"Yes",Toast.LENGTH_SHORT).show();
+        } catch (JSONException e) {
+            Toast.makeText(context,"No",Toast.LENGTH_SHORT).show();
+        }
 
-    public void writeFile(String text) {//make private
+    }
+
+    public void writeFile(String text,String path) {//make private
 
         try {
-            FileOutputStream fileOutputStream = context.openFileOutput("DrinkingAppData.txt", MODE_PRIVATE);
+            FileOutputStream fileOutputStream = context.openFileOutput(path, MODE_PRIVATE);
             fileOutputStream.write(text.getBytes());
             fileOutputStream.close();
 
@@ -95,9 +130,45 @@ public class DataStorage {
         }
     }
 
-    private String readFile() {
+    public void writeFileConsumed(String text) {//make private
+
         try {
-            FileInputStream fileInputStream = context.openFileInput("DrinkingAppData.txt");
+            FileOutputStream fileOutputStream = context.openFileOutput("DrinkingAppDataConsumed.txt", MODE_PRIVATE);
+            fileOutputStream.write(text.getBytes());
+            fileOutputStream.close();
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String readFileConsumed() {
+        try {
+            FileInputStream fileInputStream = context.openFileInput("DrinkingAppDataConsumed.txt");
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuffer stringBuffer = new StringBuffer();
+
+            String lines;
+            while ((lines = bufferedReader.readLine()) != null) {
+                stringBuffer.append(lines + "\n");
+            }
+
+            return stringBuffer.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private String readFile(String path) {
+        try {
+            FileInputStream fileInputStream = context.openFileInput(path);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
 
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
